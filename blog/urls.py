298768bf56eda8a518a112a0be0d3a4f122896df
ndiediop <ndieyed48@gmail.com>
2026-04-02 21:46:47 +0000

    Créer des templates HTML pour chaque vue

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='article-list'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('article/nouveau/', views.ArticleCreateView.as_view(), name='article-create'),
+    path('article/<int:pk>/modifier/', views.ArticleUpdateView.as_view(), name='article-update'),
+    path('article/<int:pk>/supprimer/', views.ArticleDeleteView.as_view(), name='article-delete'),
+]
